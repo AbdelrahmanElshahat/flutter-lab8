81917c3a3e043f938f3056cc172aa0aa38ea72cf
AbdelrahmanElshahat <70945443+AbdelrahmanElshahat@users.noreply.github.com>
2021-08-17 19:45:12 +0200

    Update Userdetails.dart
    Adding New features to open User location

--- lab7/lib/Screens/Userdetails.dart
+++ lab7/lib/Screens/Userdetails.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lab7/Screens/LocationScreen.dart';
 import 'package:lab7/Widget/Card.dart';
 import 'package:lab7/models/users.dart';
 
@@ -27,20 +28,18 @@ class _UserDetailsState extends State<UserDetails> {
               height: 10,
             ),
             InkWell(
-              onTap: () {
-                LaunchURL("mailto:${widget.user.email}");
-              },
-              child: Text("E-mail : ${widget.user.email}")
-            ),
+                onTap: () {
+                  LaunchURL("mailto:${widget.user.email}");
+                },
+                child: Text("E-mail : ${widget.user.email}")),
             SizedBox(
               height: 12,
             ),
             InkWell(
-              onTap: () {
-                LaunchURL("tel:${widget.user.phone}");
-              },
-              child: Text("phone : ${widget.user.phone}")
-            ),
+                onTap: () {
+                  LaunchURL("tel:${widget.user.phone}");
+                },
+                child: Text("phone : ${widget.user.phone}")),
             SizedBox(
               height: 12,
             ),
@@ -49,10 +48,14 @@ class _UserDetailsState extends State<UserDetails> {
               height: 12,
             ),
             InkWell(
-              onTap: () {
-                LaunchURL("https://${widget.user.website}");
-              },
-              child: Text("web: https://${widget.user.website}")
+                onTap: () {
+                  LaunchURL("https://${widget.user.website}");
+                },
+                child: Text("web: https://${widget.user.website}")),
+            MaterialButton(onPressed: () {
+              pushPage(context, MapSample(User_loc: widget.user.address.geo));
+            },
+            child: Text("Location"),
             ),
           ],
         ));
